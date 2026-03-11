dfa7c829a9ff9d1b1658c2059449606a13537ca0
Yande-ol <yaancarlos8@icloud.com>
2026-03-11 04:38:55 +0000

    Done

---
+++ Modulo 00/ex2/ft_hardvest_total.py
@@ -0,0 +1,8 @@
+def ft_hardvest_total():
+	Dia1 = int(input("Day 1 harvest: "))
+	Dia2 = int(input("Day 2 harvest: "))
+	Dia3 = int(input("Day 3 harvest: "))
+
+	total = Dia1 + Dia2 + Dia3
+
+	print("Total harvest:", total)
