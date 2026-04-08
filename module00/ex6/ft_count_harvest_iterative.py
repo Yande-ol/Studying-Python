3bd0b8f5a48a03e4be9ff4d7f528008277242ce2
Yande-ol <yaancarlos8@icloud.com>
2026-04-08 14:24:56 +0100

    flake8 standard correction

--- module00/ex6/ft_count_harvest_iterative.py
+++ module00/ex6/ft_count_harvest_iterative.py
@@ -1,6 +1,6 @@
 def ft_count_harvest_iterative():
-	days = int(input("Days until harvest: "))
+    days = int(input("Days until harvest: "))
 
-	for count in range(1, days + 1):
-		print("Day", count)
-	print("Harvest time!")
+    for count in range(1, days + 1):
+        print("Day", count)
+    print("Harvest time!")
